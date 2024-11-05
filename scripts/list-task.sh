80c88dbf53b244aa0567e5ebd80eeba48c1e8da8
juan.bernal <juan.bernal@novatec.com.co>
2024-11-05 09:39:53 -0500

    fix: create method for convert dates

--- scripts/list-task.sh
+++ scripts/list-task.sh
@@ -5,22 +5,26 @@ getAllTasks() {
 
   # Print all task
   echo ""
-  printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "ID" "Description" "Status" "Created At" "Updated At"
-  printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "---" "--------------------------------------------------" "-------------" "--------------" "--------------"
+  printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "ID" "Description" "Status" "Created At" "Updated At"
+  printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "---" "--------------------------------------------------" "-------------" "-------------------" "-------------------"
 
   for ((i = 0; i < length_json; i++)); do
     id=$(jq ".[$i].id" $FILE_JSON)
     description=$(jq ".[$i].description" $FILE_JSON)
     status=$(jq ".[$i].status" $FILE_JSON)
-    createdAt=$(jq ".[$i].createdAt" $FILE_JSON)
-    updateAt=$(jq ".[$i].updateAt" $FILE_JSON)
+    timestampCreatedAt=$(jq ".[$i].createdAt" $FILE_JSON)
+    timestampUpdateAt=$(jq ".[$i].updateAt" $FILE_JSON)
 
-    printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "$id" "$description" "$status" "$createdAt" "$updateAt"
-    printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "---" "--------------------------------------------------" "-------------" "--------------" "--------------"
+    createdAt=$(convertDateUnixToYMDHMS $timestampCreatedAt)
+    updateAt=$(convertDateUnixToYMDHMS $timestampUpdateAt)
+
+    printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "$id" "$description" "$status" "$createdAt" "$updateAt"
+    printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "---" "--------------------------------------------------" "-------------" "-------------------" "-------------------"
   done
   echo ""
 }
 
+#Method for filter task by todo, in-progress or done
 getTasksFilter() {
   case "$1" in
   -t)
@@ -39,20 +43,27 @@ getTasksFilter() {
 
   count=$(echo "$listFilter" | jq 'length')
 
-  # Print all task
   echo ""
-  printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "ID" "Description" "Status" "Created At" "Updated At"
-  printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "---" "--------------------------------------------------" "-------------" "--------------" "--------------"
+  printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "ID" "Description" "Status" "Created At" "Updated At"
+  printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "---" "--------------------------------------------------" "-------------" "-------------------" "-------------------"
 
   for ((i = 0; i < $count; i++)); do
     id=$(echo $listFilter | jq ".[$i].id")
     description=$(echo $listFilter | jq ".[$i].description")
     status=$(echo $listFilter | jq ".[$i].status")
-    createdAt=$(echo $listFilter | jq ".[$i].createdAt")
-    updateAt=$(echo $listFilter | jq ".[$i].updateAt")
+    timestampCreatedAt=$(echo $listFilter | jq ".[$i].createdAt")
+    timestampUpdateAt=$(echo $listFilter | jq ".[$i].updateAt")
 
-    printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "$id" "$description" "$status" "$createdAt" "$updateAt"
-    printf "| %-3s | %-50s | %-13s | %-14s | %-14s |\n" "---" "--------------------------------------------------" "-------------" "--------------" "--------------"
+    createdAt=$(convertDateUnixToYMDHMS $timestampCreatedAt)
+    updateAt=$(convertDateUnixToYMDHMS $timestampUpdateAt)
+
+    printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "$id" "$description" "$status" "$createdAt" "$updateAt"
+    printf "| %-3s | %-50s | %-13s | %-19s | %-19s |\n" "---" "--------------------------------------------------" "-------------" "-------------------" "-------------------"
   done
   echo ""
+}
+
+# Method for convert timestamp to date complete
+convertDateUnixToYMDHMS() {
+  date -d "@$1" +"%Y-%m-%d %H:%M:%S"
 }
